58e8ce1e000ec45d18944b8dbaac59a17e6cd047
samah_mustafa <i.samaaah@gmail.com>
2020-08-02 02:53:53 +0300

    Updates

---
+++ precache-manifest.9d366aa60f18bb38236c0330f81bfe68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b4408620addd351431e3f2d811c24b",
+    "url": "/smah-folio/index.html"
+  },
+  {
+    "revision": "2884d7f95bea78b4471b",
+    "url": "/smah-folio/static/css/main.50cb59b2.chunk.css"
+  },
+  {
+    "revision": "d8032bb57b0e7f2ad611",
+    "url": "/smah-folio/static/js/2.113052ad.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/smah-folio/static/js/2.113052ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2884d7f95bea78b4471b",
+    "url": "/smah-folio/static/js/main.8a52439a.chunk.js"
+  },
+  {
+    "revision": "37ee5d9f61612a5ae8c6",
+    "url": "/smah-folio/static/js/runtime-main.d4bbf391.js"
+  }
+]);
